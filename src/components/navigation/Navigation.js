f1be42d30261b6c9c99977969f8fb69cfd832a7b
Michael <mball.28@outlook.com>
2019-05-13 14:35:10 -0600

    color

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -8,6 +8,8 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
             <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signin')}
                     className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+                <p onClick={() => onRouteChange('signin')}
+                className='f3 link dim black underline pa3 pointer'>Delete Account</p>
             </nav>
         )
 
